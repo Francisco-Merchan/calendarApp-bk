b22cf591b2fbcc9ee90378bf74bee9511f2b91ee
Francisco Merchan <merchanf6@gmail.com>
2023-07-14 16:17:52 -0300

    hotfix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 // Directorio Publico
 // app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { extensions: ["js"] }));
 
 // Agrega los siguientes encabezados para permitir el uso de módulos JavaScript
 app.use((req, res, next) => {
